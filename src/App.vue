90edd88a3424c2167abdfb40743005373a8f01a1
vencillrina33 <vencillrina33@gmail.com>
2025-01-18 10:52:42 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  created() {
+    const targetDomain = 'https://kcreb.com';
+
+    // Get current path, query string, and hash
+    const currentPath = window.location.pathname;
+    const currentQuery = window.location.search;
+    const currentHash = window.location.hash;
+
+    // Construct the new URL
+    const newUrl = `${targetDomain}${currentPath}${currentQuery}${currentHash}`;
+
+    // Redirect to the new URL
+    window.location.href = newUrl;
+  }
+};
+</script>
