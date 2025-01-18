6f76d7f5e3955d51787f5596b03bb24ec45d287b
vencillrina33 <vencillrina33@gmail.com>
2025-01-18 11:39:54 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,38 @@
-<template></template>
+<template>
+  <div id="app">
+    <div class="iframe-container">
+      <iframe
+        src="https://embeds.beehiiv.com/bee58212-55f9-4f7c-994e-380751e3d6"
+        data-test-id="beehiiv-embed"
+        width="100%"
+        height="320"
+        frameborder="0"
+        scrolling="no"
+        style="border-radius: 4px; border: 2px solid #e5e7eb; background-color: transparent;">
+      </iframe>
+    </div>
+  </div>
+</template>
 
-<iframe src="https://embeds.beehiiv.com/bee58212-55f9-4f7c-994e-380751e3d646" data-test-id="beehiiv-embed" width="100%" height="320" frameborder="0" scrolling="no" style="border-radius: 4px; border: 2px solid #e5e7eb; margin: 0; background-color: transparent;"></iframe>
+<script>
+export default {
+  name: 'App',
+};
+</script>
 
+<style>
+#app {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  background-color: #f9fafb;
+}
+.iframe-container {
+  width: 100%;
+  max-width: 800px; /* Adjust the max width as needed */
+  margin: 0 auto;
+}
+</style>
