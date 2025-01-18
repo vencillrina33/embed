d2c6baeb3251443057d973ccffb76440cad1c157
vencillrina33 <vencillrina33@gmail.com>
2025-01-18 10:57:10 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,4 @@
 <template></template>
 
-<script>
-export default {
-  created() {
-    const targetDomain = 'https://kcreb.com';
+<iframe src="https://embeds.beehiiv.com/bee58212-55f9-4f7c-994e-380751e3d646" data-test-id="beehiiv-embed" width="100%" height="320" frameborder="0" scrolling="no" style="border-radius: 4px; border: 2px solid #e5e7eb; margin: 0; background-color: transparent;"></iframe>
 
-    // Get current path, query string, and hash
-    const currentPath = window.location.pathname;
-    const currentQuery = window.location.search;
-    const currentHash = window.location.hash;
-
-    // Construct the new URL
-    const newUrl = `${targetDomain}${currentPath}${currentQuery}${currentHash}`;
-
-    // Redirect to the new URL
-    window.location.href = newUrl;
-  }
-};
-</script>
